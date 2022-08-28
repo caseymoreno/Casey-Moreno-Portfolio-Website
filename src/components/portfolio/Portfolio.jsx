af1f2693c20185fd3ffe8b40118c0feddb1b1849
Casey <casey.moreno0012@gmail.com>
2022-08-28 19:37:23 -0400

    updating portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import './portfolio.css'
 import WORDLY from '../../assets/Wordly Guess.png'
 import JC from '../../assets/J&C-Pools.png'
 import YELPCAMP from '../../assets/YelpCamp.png'
-
+import FLPRICE from "../../assets/FloridaHomePricePrediction.png"
 
 const Portfolio = () => {
   return (
@@ -18,7 +18,7 @@ const Portfolio = () => {
           </div>
           <h3>YelpCamp</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com/caseymoreno/J-C-Pools" className='btn'>Github</a>
+            <a href="https://github.com/caseymoreno/YelpCamp" className='btn'>Github</a>
             <a href="https://boiling-bastion-71782.herokuapp.com/" className='btn btn-primary'>Live Site</a>
           </div>
         </article>
@@ -30,6 +30,16 @@ const Portfolio = () => {
           <div className="portfolio__item-cta">
             <a href="https://github.com/caseymoreno/Wordly-Guesss.git" className='btn'>Github</a>
             <a href="https://wordlyguess.netlify.app/" className='btn btn-primary'>Live Site</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={JC} alt="Wordly Guess Web App" />
+          </div>
+          <h3>Florida Home Price Prediction</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/caseymoreno/FloridaHomePricePrediction" className='btn'>Github</a>
+            <a href="https://www.jandcfiberglasspool.com" className='btn btn-primary'>Live Site</a>
           </div>
         </article>
         <article className='portfolio__item'>
